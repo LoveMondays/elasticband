ec0b19d2bdc597a3c3621cd4893fd283edd8b7fc
Glauber Campinho <glauber@lovemondays.com.br>
2015-07-06 15:47:19 -0300

    Add more specs to `Search` to check when the aggregation or query is empty

--- lib/elasticband/search.rb
+++ lib/elasticband/search.rb
@@ -7,10 +7,11 @@ module Elasticband
       # #### Examples
       # ```
       # Search.parse('foo', on: :name, group_by: :status)
-      # => { query: { match: { name: 'foo' } }, aggr: { status: { terms: { field: :status } } } }
+      # => { query: { match: { name: 'foo' } }, aggs: { status: { terms: { field: :status } } } }
       # ```
       def parse(query_text, options)
-        { query: Query.parse(query_text, options), aggr: Aggregation.parse(options) }
+        hash = { query: Query.parse(query_text, options), aggs: Aggregation.parse(options) }
+        hash.reject { |_, value| value.blank? }
       end
     end
   end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -5,10 +5,29 @@ RSpec.describe Elasticband::Search do
     subject { described_class.parse('foo', on: :name, group_by: :status) }
 
     before do
-      allow(Elasticband::Query).to receive(:parse).with('foo', on: :name, group_by: :status) { 'query' }
-      allow(Elasticband::Aggregation).to receive(:parse).with(on: :name, group_by: :status) { 'aggregation' }
+      allow(Elasticband::Query).to receive(:parse).with('foo', on: :name, group_by: :status) { query }
+      allow(Elasticband::Aggregation).to receive(:parse).with(on: :name, group_by: :status) { aggregation }
     end
 
-    it { is_expected.to eq(query: 'query', aggr: 'aggregation') }
+    context 'with only query' do
+      let(:query) { 'query' }
+      let(:aggregation) { {} }
+
+      it { is_expected.to eq(query: 'query') }
+    end
+
+    context 'with only aggregation' do
+      let(:query) { {} }
+      let(:aggregation) { 'aggregation' }
+
+      it { is_expected.to eq(aggs: 'aggregation') }
+    end
+
+    context 'with query and aggregation' do
+      let(:query) { 'query' }
+      let(:aggregation) { 'aggregation' }
+
+      it { is_expected.to eq(query: 'query', aggs: 'aggregation') }
+    end
   end
 end
